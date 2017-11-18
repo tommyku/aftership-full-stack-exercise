d4c9319e6139c96fb11900068998a1103bbb8922
Tommy Ku <zoo1929@outlook.com>
2017-11-18 20:58:10 +0800

    ADD: additional server depencies and eslint

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
-const app = require('express')();
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// Logging requests to STDOUT
+app.use(logger('dev'));
+
+// Parse incoming data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Wrong castle');
